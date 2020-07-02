d180957b8d4c48750f023dc3a74bf6957c3c4946
Tabula <MrZhnex@ya.ru>
2020-07-03 00:17:01 +0300

    Upload

---
+++ CustomChangeWallpaper/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace CustomChangeWallpaper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picThumbnail = new System.Windows.Forms.PictureBox();
+            this.openGraphic = new System.Windows.Forms.OpenFileDialog();
+            this.createImage = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picThumbnail)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picThumbnail
+            // 
+            this.picThumbnail.Location = new System.Drawing.Point(223, 56);
+            this.picThumbnail.Name = "picThumbnail";
+            this.picThumbnail.Size = new System.Drawing.Size(174, 134);
+            this.picThumbnail.TabIndex = 2;
+            this.picThumbnail.TabStop = false;
+            // 
+            // openGraphic
+            // 
+            this.openGraphic.FileName = "openFileDialog1";
+            // 
+            // createImage
+            // 
+            this.createImage.Location = new System.Drawing.Point(12, 108);
+            this.createImage.Name = "createImage";
+            this.createImage.Size = new System.Drawing.Size(137, 50);
+            this.createImage.TabIndex = 3;
+            this.createImage.Text = "Создать изображения в количестве штук:";
+            this.createImage.UseVisualStyleBackColor = true;
+            this.createImage.Click += new System.EventHandler(this.CreateImage_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(26, 164);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(35, 20);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(256, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(99, 164);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(29, 20);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.Text = "1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(424, 203);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.createImage);
+            this.Controls.Add(this.picThumbnail);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picThumbnail)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox picThumbnail;
+        private System.Windows.Forms.OpenFileDialog openGraphic;
+        private System.Windows.Forms.Button createImage;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ CustomChangeWallpaper/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.IO;
+using Microsoft.Win32;
+using System.Windows.Media.Imaging;
+
+namespace CustomChangeWallpaper
+{
+    public partial class Form1 : Form
+    {
+        private string CurrentPath;
+        private string WallpaperFolder = @"\Wallpaper\";
+        private Bitmap BM = new Bitmap(Screen.PrimaryScreen.Bounds.Width / 10, Screen.PrimaryScreen.Bounds.Height / 10);
+        List<Thread> al = new List<Thread>();
+        private int globalCountScreen = 0;
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+
+        private bool Flag
+        {
+            get
+            {
+                return flag;
+            }
+            set
+            {
+                flag = value;
+            }
+        }
+        private bool flag = true;
+
+        private Color[] colors;
+        private Color[] colorsBlackWhite;
+        private int previosSecond;
+        private int currentCountBmp = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ChangeWallpaper()
+        {
+            while (true)
+            {
+                if (Flag)
+                {                 
+                    previosSecond = DateTime.Now.Second;
+                    picThumbnail.ImageLocation = CurrentPath + WallpaperFolder + globalCountScreen + ".bmp";
+                    SetWallpaper(CurrentPath + WallpaperFolder + globalCountScreen + ".bmp", 2, 0);
+                    globalCountScreen = globalCountScreen + 1;
+                  
+                    if (globalCountScreen >= currentCountBmp)
+                    {
+                        globalCountScreen = 0;
+                    }
+
+                }
+            }
+        }
+        private void SetWallpaper(string WallpaperLocation, int WallpaperStyle, int TileWallpaper)
+        {
+            SystemParametersInfo(20, 0, WallpaperLocation, 0x01 | 0x02);
+            RegistryKey rkWallPaper = Registry.CurrentUser.OpenSubKey("Control Panel\\Desktop", true);
+            rkWallPaper.SetValue("WallpaperStyle", WallpaperStyle);
+            rkWallPaper.SetValue("TileWallpaper", TileWallpaper);
+            rkWallPaper.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            previosSecond = DateTime.Now.Second;
+            colors = new Color[] { Color.Red, Color.Blue, Color.Green, Color.Yellow, Color.Aqua, Color.Magenta, Color.Orange, Color.ForestGreen, Color.Coral };
+            colorsBlackWhite = new Color[] { Color.Black, Color.White };
+            picThumbnail.SizeMode = PictureBoxSizeMode.Zoom;
+            picThumbnail.ImageLocation = GetCurrentWallpaper();
+            CurrentPath = Application.StartupPath;
+            if (!Directory.Exists(CurrentPath + WallpaperFolder))
+                Directory.CreateDirectory(CurrentPath + WallpaperFolder);
+            DirectoryInfo dirInfo = new DirectoryInfo(CurrentPath + WallpaperFolder);          
+            foreach (FileInfo file in dirInfo.GetFiles())
+            {
+                currentCountBmp = currentCountBmp + 1;
+            }
+            Thread mainThread = new Thread(ChangeWallpaper);
+            al.Add(mainThread);
+            mainThread.Start();
+        }
+
+        private string GetCurrentWallpaper()
+        {
+            RegistryKey rkWallPaper = Registry.CurrentUser.OpenSubKey("Control Panel\\Desktop", false);
+            string WallpaperPath = rkWallPaper.GetValue("WallPaper").ToString();
+            rkWallPaper.Close();
+            return WallpaperPath;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            foreach (Thread t in al)
+            {
+                t.Abort();
+            }
+        }
+
+        private void CreateImage_Click(object sender, EventArgs e)
+        {
+            Flag = false;
+            if (!Flag)
+            {
+                DirectoryInfo dirInfo = new DirectoryInfo(CurrentPath + WallpaperFolder);
+                foreach (FileInfo file in dirInfo.GetFiles())
+                {
+                    file.Delete();
+                }
+                currentCountBmp = 0;
+                globalCountScreen = 0;
+                for (int i = 0; i < Convert.ToInt16(textBox1.Text); i++)
+                {
+                    Random rand = new Random();
+
+                    for (int y = 0; y < Screen.PrimaryScreen.Bounds.Height / 10; y++)
+                    {
+                        for (int x = 0; x < Screen.PrimaryScreen.Bounds.Width / 10; x++)
+                        {
+                            Color tempColor = Color.Olive;
+                            if (Convert.ToInt16(textBox2.Text) == 1)
+                                tempColor = colors[rand.Next(0, 9)];
+                            else if (Convert.ToInt16(textBox2.Text) == 2)
+                                tempColor = colorsBlackWhite[rand.Next(0, 2)];
+                            BM.SetPixel(x, y, tempColor);
+                        }
+                    }
+                    BM.Save(CurrentPath + WallpaperFolder + currentCountBmp + ".bmp", System.Drawing.Imaging.ImageFormat.Bmp);
+                    currentCountBmp = currentCountBmp + 1;
+                    SetWallpaper(CurrentPath + WallpaperFolder + currentCountBmp + ".bmp", 2, 0);
+                    picThumbnail.ImageLocation = CurrentPath + WallpaperFolder + currentCountBmp + ".bmp";
+                }
+            }
+            Flag = true;
+        }
+    }
+}
